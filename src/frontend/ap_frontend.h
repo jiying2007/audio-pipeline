1e0f526d0c388741456f613fef16d5c04f794bb1
jiying2007 <33591504+jiying2007@users.noreply.github.com>
2026-09-05 16:31:32 +0800

    bf: track hard microphone contamination mode

--- src/frontend/ap_frontend.h
+++ src/frontend/ap_frontend.h
@@ -26,6 +26,7 @@ typedef struct ap_beamformer_state {
     uint32_t fallback_active;
     uint32_t fallback_strong_channel;
     uint32_t fallback_recovery_count;
+    uint32_t fallback_hard_fault;
 } ap_beamformer_state_t;
 
 void ap_hpf_init(ap_hpf_state_t *state,
